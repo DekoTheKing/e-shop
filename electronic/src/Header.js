33555cc93d3d9670c370cc1fbc4810c8e3e684c2
Dejan Serafimovski <serafimovskidejan@gmail.com>
2024-01-20 00:33:51 +0100

    Final version

--- electronic/src/Header.js
+++ electronic/src/Header.js
@@ -1,22 +1,25 @@
 // Header.js
+
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './styles.css';
 
 function Header() {
   return (
-    <div className="header"> {/* Add a div wrapper */}
+    <div className="header">
       <nav>
         <ul>
           <li><Link to="/">Home</Link></li>
           <li><Link to="/products">Products</Link></li>
           <li><Link to="/about">About Us</Link></li>
           <li><Link to="/cart">Cart</Link></li>
-          <li><Link to="/login">Login</Link></li>
-          <li><Link to="/signin">Sign In</Link></li>
         </ul>
       </nav>
       <h1>Welcome to our Electronic Store</h1>
+      <div className="login-buttons">
+        <Link to="/login">Login</Link>
+        <Link to="/signin">Sign In</Link>
+      </div>
     </div>
   );
 }
